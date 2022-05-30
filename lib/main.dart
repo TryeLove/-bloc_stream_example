28436426978d0851fb4d80f37addd30209d5b6ee
TrueLove <kulakevichyashca@gmail.com>
2022-05-30 11:39:16 +0300

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Bloc with Stream'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: AnimatedContainer(
+          height: 100,
+          width: 100,
+          color: Colors.red,
+          duration: Duration(microseconds: 500),
+        ),
+      ),
+    );
+  }
+}
+
